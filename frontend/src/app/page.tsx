399b597bce452167744192f23a21ab9dc733bb11
Ruben Tanoey <92169008+rubentanoey@users.noreply.github.com>
2023-12-04 00:27:33 +0700

    to be deployed! [All Case Done]

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,7 +4,7 @@ import React, { useCallback } from "react";
 import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
-import { Button, TextField, Dropdown } from "../components/elements";
+import { Button, TextField, Dropdown, Container } from "../components/elements";
 import axios from "axios";
 
 export default function Home() {
@@ -13,6 +13,16 @@ export default function Home() {
   const [dateTime, setDateTime] = useState(new Date());
   const [searchValue, setSearchValue] = useState("");
   const [methodValue, setMethodValue] = useState("");
+  const [historyData, setHistoryData] = useState({});
+
+  useEffect(() => {
+    const deviceId = localStorage.getItem("device_id");
+
+    if (!deviceId) {
+      const newDeviceId = Math.random().toString(36).substring(2, 15);
+      localStorage.setItem("device_id", newDeviceId);
+    }
+  }, []);
 
   const hour = dateTime.getHours();
   let greeting: string;
@@ -69,63 +79,114 @@ export default function Home() {
     }
   };
 
+  const handleHistory = async () => {
+    try {
+      const response = await axios.get(`http://35.219.64.188/history`, {
+        params: {
+          device_id: localStorage.getItem("device_id"),
+        },
+      });
+      setHistoryData(response.data.data);
+      console.log("History success:", response.data);
+    } catch (error) {
+      console.error("History error:", error);
+    }
+  };
+
+  useEffect(() => {
+    handleHistory();
+  }, []);
+
+  const formatTime = (time: string) => {
+    const timeStr = time.split(" ")[1];
+    const timeOnly = timeStr.replaceAll("-", ":");
+    return timeOnly;
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-5 p-24 bg-primary">
-      <div className="flex flex-col gap-2 items-center">
-        <div
-          className="text-7xl text-primaryText font-black"
-          suppressHydrationWarning
-        >
-          {dateTime.toLocaleString("en-US", { timeStyle: "short" })}
+    <main className="flex min-h-screen items-center justify-center gap-8 p-24 bg-primary">
+      
+      <div className="w-full flex flex-col items-center justify-center gap-5">
+        <div className="flex flex-col gap-2 items-center">
+          <div
+            className="text-7xl text-primaryText font-black"
+            suppressHydrationWarning
+          >
+            {dateTime.toLocaleString("en-US", { timeStyle: "short" })}
+          </div>
+          <div className="text-5xl text-secondaryText font-semibold">
+            {greeting}
+          </div>
         </div>
-        <div className="text-5xl text-secondaryText font-semibold">
-          {greeting}
+
+        <div className="flex flex-col w-full justify-center items-center">
+          <Image
+            src="/Cradren.svg"
+            alt="Cradren"
+            width={100}
+            height={100}
+          ></Image>
+
+          <div className="relative top-[-42px] flex flex-row w-full bg-white py-3 px-5 items-center justify-center rounded-full">
+            <TextField
+              className="w-full"
+              placeholder="You are safe to pry here :)"
+              value={searchValue}
+              onChange={handleSearchChange}
+            />
+
+            <div className="flex flex-row gap-2 items-center justify-center">
+              <Dropdown
+                className="w-40"
+                placeholder="Select Method"
+                value={methodValue}
+                onChange={handleMethodChange}
+              ></Dropdown>
+              <svg
+                width="2"
+                height="30"
+                viewBox="0 0 2 30"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <line
+                  x1="0.5"
+                  y1="30"
+                  x2="0.5"
+                  y2="0.5"
+                  stroke="#CBE04C"
+                  strokeWidth="2"
+                />
+              </svg>
+              <Button
+                className="bg-primaryText py-2 px-5"
+                onClick={handleSearchClick}
+              >
+                Search
+              </Button>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="flex flex-col w-full justify-center items-center">
-        <Image
-          src="/Cradren.svg"
-          alt="Cradren"
-          width={100}
-          height={100}
-        ></Image>
-
-        <div className="relative top-[-42px] flex flex-row w-[60%] bg-white py-3 px-5 items-center justify-center rounded-full">
-          <TextField
-            className="w-full"
-            placeholder="You are safe to pry here :)"
-            value={searchValue}
-            onChange={handleSearchChange}
-          />
-
-          <div className="flex flex-row gap-2 items-center justify-center">
-            <Dropdown
-              className="w-40"
-              placeholder="Select Method"
-              value={methodValue}
-              onChange={handleMethodChange}
-            ></Dropdown>
-            <svg
-              width="2"
-              height="30"
-              viewBox="0 0 2 30"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <line
-                x1="0.5"
-                y1="30"
-                x2="0.5"
-                y2="0.5"
-                stroke="#CBE04C"
-                strokeWidth="2"
-              />
-            </svg>
-            <Button className="py-2 px-5" onClick={handleSearchClick}>
-              Search
-            </Button>
-          </div>
+      <div className="w-[25%] h-[500px] overflow-y-auto rounded-2xl">
+        <div className="flex flex-col w-full justify-center items-center gap-4">
+          {Object.keys(historyData).map((dateKey) => (
+            <Container className="flex-col w-full gap-2" key={dateKey}>
+              <div className="text-primaryText text-lg font-bold">{dateKey}</div>
+              {historyData[dateKey].map((item, index) => (
+                <div className="flex w-full" key={index}>
+                  <div className="flex w-full justify-between gap-4">
+                    <div className="text-stone-500 text-base font-semibold">
+                      {item.query}
+                    </div>
+                    <div className="text-stone-400 text-sm font-normal">
+                      {formatTime(item.time)}
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </Container>
+          ))}
         </div>
       </div>
     </main>
